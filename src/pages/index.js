04866811dadea5807d39dfa33950aa126f017fca
Aayush Pandey <aayushpandey1100@gmail.com>
2023-04-01 11:12:48 +0530

    Amazon Clone

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Header from "../components/header";
+import Banner from "../components/banner";
+import ProductFeed from "../components/productfeed";
+
+export default function Home({products}) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      <Header/>
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+        <Banner/>
+
+        {/* product feed */}
+        <ProductFeed
+        products={products}
+        />
+      </main>
+    </div>
+  );
+}
+export async function getServerSideProps(context){
+const products =await fetch('https://fakestoreapi.com/products').then(
+  (res)=>res.json()
+);
+return{
+  props:{
+    products,
+  }
+}
+}
+
+// https://fakestoreapi.com/products
