a37f4703c9a2465f122560f86fa8819a0265d912
Aayush Pandey <aayushpandey1100@gmail.com>
2023-04-03 14:47:47 +0530

    checkout page using redux

--- src/components/product.js
+++ src/components/product.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import {StarIcon} from '@heroicons/react/solid'
 // import reactCurrencyFormatter from 'react-currency-formatter';
 import Currency from 'react-currency-formatter';
+import { addToBasket } from '../slices/basketSlice';
+import { useDispatch } from 'react-redux';
 const MAX_RATING=5;
 const MIN_RATING=1;
 function Product({id,title,price,description,category,image}) {
@@ -12,6 +14,22 @@ function Product({id,title,price,description,category,image}) {
     const [isPrime]=useState(
         Math.random()<0.5
     )
+
+      const dispatch=useDispatch()
+    const addTobasket=()=>{
+      const product={
+        id,
+        title,
+        price,
+        description,
+        category,
+        image,
+        isPrime,
+        rating
+      }
+      // sending the product as an action to slice
+      dispatch(addToBasket(product))
+    }
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className='absolute top-2 right-2 italic text-grey-400'>{category}</p>
@@ -35,7 +53,7 @@ function Product({id,title,price,description,category,image}) {
             <p className='text-xs text-gray-500'>FREE Next-Day Delivery</p>
           </div>
         )}
-        <button className='mt-auto button'>Add To Basket</button>
+        <button onClick={addTobasket} className='mt-auto button'>Add To Basket</button>
     </div>
   )
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux"
 import Header from "../components/header"
 import Image from "next/image"
-
+import { selectItems, selectTotal } from "../slices/basketSlice"
+import CheckoutProduct from "../components/checkoutProduct";
+import Currency from 'react-currency-formatter'
+import { useSession } from "next-auth/react";
 function Checkout() {
+    const items=useSelector(selectItems);
+    const {data:session,event}=useSession()
+    const total=useSelector(selectTotal);
   return (
     <div className="bg-gray-100">
         <Header/>
@@ -17,16 +24,50 @@ function Checkout() {
                 />
 
 <div className="flex flex-col p-5 space-y-10 bg-white ">
-                <h1 className="text-3xl">Your Shopping Basket</h1>
-            </div>
+                <h1 className="text-3xl">{items.length===0?'Your cart is empty':`Checkout ${items.length} items`}</h1>
+            
 
+            {   items.length>0&&
+                items.map((item,i)=>{
+                    return <CheckoutProduct
+                    key={i}
+                    id={item.id}
+                    title={item.title}
+                    price={item.price}
+                    description={item.description}
+                    category={item.category}
+                    image={item.image}
+                    isPrime={item.isPrime}
+                    rating={item.rating}
+                    />
+                })
+            }
 
-
+</div>
             </div>
             
 
             {/* right */}
-            <div></div>
+            <div className="flex bg-white p-10 flex-col shadow-md">
+                {
+                    items.length>0&&(
+                        <>
+                        <h2 className="whitespace-nowrap">
+                            Subtotal ({items.length} items:)
+                            <span className="font-bold">
+                             <Currency quantity={total*80} currency="INR"/>   
+                            </span>
+                        </h2>
+                        <button
+                        disabled={!session}
+                        className={`button whitespace-nowrap mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-700 cursor-not-allowed' }`}
+                        >
+                            {!session?'SignIn to Checkout':'Proceed to Checkout'}
+                        </button>
+                        </>
+                    )
+                }
+            </div>
         </main>
     </div>
   )
