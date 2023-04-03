df9d8da2e37bc452cb1a1afaed1e07877db69cd0
Aayush Pandey <aayushpandey1100@gmail.com>
2023-04-03 09:54:38 +0530

    google auth

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ-XYc1SpwSvxdEQ6QeMQJbI-4y9RqGRc",
+    authDomain: "clone-2508f.firebaseapp.com",
+    projectId: "clone-2508f",
+    storageBucket: "clone-2508f.appspot.com",
+    messagingSenderId: "1078141082581",
+    appId: "1:1078141082581:web:9e297510a1589c31edfb5c",
+    measurementId: "G-XPS6K8YQ6V"
+  };
+  const app = initializeApp(firebaseConfig);
--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,18 @@ import {
   SearchIcon,
   ShoppingCartIcon,  
 }from "@heroicons/react/outline"
+import {signIn,signOut,useSession} from 'next-auth/react';
+import {useRouter} from 'next/router'
 export default function Header() {
+  const { data: session, status } = useSession()
+  const router=useRouter()
   return (
     <header>
-
       {/* top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
             <Image
+            onClick={()=>router.push('/')}
                 src="https://links.papareact.com/f90"
                 width={150}
                 height={40}
@@ -21,7 +25,7 @@ export default function Header() {
         </div>
 
 
-        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 ">
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
             <input 
             type="text"
             className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
@@ -31,8 +35,8 @@ export default function Header() {
 
 
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-            <div className=" link">
-                <p>Aayush Pandey</p>
+            <div onClick={!session ? signIn : signOut} className=" link">
+                <p>{session ? `Hello ${session.user.name}`:"SignIn"}</p>
                 <p className="font-extrabold md:text-sm">Account & Lists</p>
             </div>
             <div className=" link">
